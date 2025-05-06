6d94d0348a132fce94f6db0bb91cb35c4bbc7d32
Andrés Contreras Guillén <andres.contreras@getfirefly.io>
2025-05-06 23:43:26 +0200

    Refactor and extend plugin extension point configurations
    Moved `SignaturePluginExtensionPointConfig` to a new package with added logging functionality. Introduced `DocumentStoragePluginExtensionPointConfig` to register a new extension point for storage providers, ensuring improved modularity and maintainability.

--- common-platform-document-mgmt-core/src/main/java/com/catalis/commons/ecm/core/extensions/config/SignaturePluginExtensionPointConfig.java
+++ common-platform-document-mgmt-core/src/main/java/com/catalis/commons/ecm/core/extensions/config/SignaturePluginExtensionPointConfig.java
@@ -1,12 +1,14 @@
-package com.catalis.commons.ecm.core.extensions;
+package com.catalis.commons.ecm.core.extensions.config;
 
 import com.catalis.commons.ecm.interfaces.extensions.SignatureProviderExtension;
 import com.catalis.core.plugin.api.PluginManager;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class SignaturePluginExtensionPointConfig {
 
     @Autowired
@@ -14,11 +16,14 @@ public class SignaturePluginExtensionPointConfig {
 
     @PostConstruct
     public void registerSignatureProviderExtension() {
+        log.info("Registering signature provider extension point");
         pluginManager.getExtensionRegistry()
                 .registerExtensionPoint(
                         "com.catalis.commons.ecm.interfaces.extensions.signature-provider",
                         SignatureProviderExtension.class
                 )
                 .subscribe();
+        log.info("Signature provider extension point registered successfully");
     }
+
 }
